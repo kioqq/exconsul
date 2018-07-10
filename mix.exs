528a7d9ad95cbe78714d34bed1228c690bce8dc0
Evgeniy Evgrafov <kio@fx-s.pro>
2018-07-10 19:57:03 +0300

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Consul.Mixfile do
   def project do
     [
       app: :exconsul,
-      version: "1.0.0",
+      version: "1.0.1",
       elixir: "~> 1.6",
       deps: deps(),
       package: package(),
